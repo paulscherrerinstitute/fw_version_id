6001e587f6f36a15b22ded4a7804de2a737b87ef
stef_b <benoit.stef@psi.ch>
2023-03-01 19:08:24 +0100

    REFACTOR: Adapt to new library version

--- VivadoIP/drivers/fw_version_id/data/fw_version_id.tcl
+++ VivadoIP/drivers/fw_version_id/data/fw_version_id.tcl
@@ -1,5 +1,5 @@
 
 
 proc generate {drv_handle} {
-	xdefine_include_file $drv_handle "xparameters.h" fw_version_id "NUM_INSTANCES" "DEVICE_ID"  "C_BASEADDR" "C_HIGHADDR"
+	xdefine_include_file $drv_handle "xparameters.h" fw_version_id "NUM_INSTANCES" "DEVICE_ID"  "C_BASEADDR" "C_HIGHADDR" 
 }
